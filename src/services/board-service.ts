3b08ce19c13bc24ce432336f583c840b7715c50c
Kitorosano <rosanoesteban2@gmail.com>
2025-02-07 02:54:51 -0300

    refactor: optimizar funciones de clases de fila y columna en servicios de ajedrez

--- src/services/board-service.ts
+++ src/services/board-service.ts
@@ -1,28 +1,26 @@
 import SquareModel from "../models/SquareModel";
 
-const rowStartClasses = (index: number) =>
-  ({
-    1: "row-start-1",
-    2: "row-start-2",
-    3: "row-start-3",
-    4: "row-start-4",
-    5: "row-start-5",
-    6: "row-start-6",
-    7: "row-start-7",
-    8: "row-start-8",
-  }[index]);
+const rowStartClasses: Record<number, string> = {
+  1: "row-start-1",
+  2: "row-start-2",
+  3: "row-start-3",
+  4: "row-start-4",
+  5: "row-start-5",
+  6: "row-start-6",
+  7: "row-start-7",
+  8: "row-start-8",
+};
 
-const colStartClasses = (index: number) =>
-  ({
-    1: "col-start-1",
-    2: "col-start-2",
-    3: "col-start-3",
-    4: "col-start-4",
-    5: "col-start-5",
-    6: "col-start-6",
-    7: "col-start-7",
-    8: "col-start-8",
-  }[index]);
+const colStartClasses: Record<number, string> = {
+  1: "col-start-1",
+  2: "col-start-2",
+  3: "col-start-3",
+  4: "col-start-4",
+  5: "col-start-5",
+  6: "col-start-6",
+  7: "col-start-7",
+  8: "col-start-8",
+};
 
 export const getSquareStyles = (
   square: SquareModel,
@@ -30,8 +28,8 @@ export const getSquareStyles = (
 ) => {
   const rowIndex = playingAsWhite ? 8 - square.row : square.row + 1;
   const colIndex = playingAsWhite ? square.column + 1 : 8 - square.column;
-  const rowStartStyle = rowStartClasses(rowIndex);
-  const colStartStyle = colStartClasses(colIndex);
+  const rowStartStyle = rowStartClasses[rowIndex];
+  const colStartStyle = colStartClasses[colIndex];
 
   return `${rowStartStyle} ${colStartStyle}`;
 };

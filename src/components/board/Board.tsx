e405e31930084654f0bb6005732b0f86a5705398
Kitorosano <rosanoesteban2@gmail.com>
2025-02-07 11:47:19 -0300

    refactor: mejorar estilos y estructura del componente Board en el juego de ajedrez

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -12,15 +12,15 @@ function Board({ board, playingAsWhite }: Props) {
   return (
     <>
       <h2>Board</h2>
-      <section className="grid grid-rows-8 grid-cols-8">
+      <section className="grid grid-rows-8 grid-cols-8 w-3/4 aspect-square m-auto my-4 border shadow">
         {board.squares.map((square: SquareModel) => {
           const squareStyles = getSquareStyles(square, playingAsWhite);
           return (
-            <div className={squareStyles}>
-              <Square
-                key={`square_${square.row}_${square.column}`}
-                square={square}
-              />
+            <div
+              key={`square_${square.row}_${square.column}`}
+              className={`${squareStyles} h-full wf-full`}
+            >
+              <Square square={square} />
             </div>
           );
         })}

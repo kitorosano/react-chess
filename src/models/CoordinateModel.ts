0dce210059d03cc5e609adec6180a7bbd7b20a6e
Kitorosano <rosanoesteban2@gmail.com>
2025-02-08 11:45:28 -0300

    feat: agregar métodos para establecer fila y columna en el modelo de coordenadas

--- src/models/CoordinateModel.ts
+++ src/models/CoordinateModel.ts
@@ -7,6 +7,14 @@ export class CoordinateModel {
     this.column = column;
   }
 
+  setRow(row: number) {
+    this.row = row;
+  }
+
+  setColumn(column: number) {
+    this.column = column;
+  }
+
   isEquals(coordinate: CoordinateModel): boolean {
     return this.row === coordinate.row && this.column === coordinate.column;
   }
